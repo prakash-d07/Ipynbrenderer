c8ee6bcd5ec15d76c2b3cc843f62d427c1849f12
prakash <saiprakash2488@gmail.com>
2022-09-18 16:58:43 -0400

    skeleton updated

--- setup.py
+++ setup.py
@@ -15,14 +15,12 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A Python Package"
+    description="A Python Package",
     long_description=long_description,
-    long_description.content="text/markdown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={'','src'},
-    packages=setuptools.find_packages(where='src')
-
-)
+    packages=setuptools.find_packages(where='src'))
